721bd0c4bbd0c5e8b0ce40e48700cda83fd617ea
jason-xiao <jaSon6787forgItlab>
2022-04-06 18:12:56 +0800

    <0003><c>

---
+++ solution/0001-0099/0003. Longest Substring Without Repeating Characters/solution.c
@@ -0,0 +1,25 @@
+int lengthOfLongestSubstring(char* s) {
+  int   result = 0;
+  char* b      = s;
+  char* c      = s + 1;
+  while (*s != 0) {
+    if (*c == 0) {
+      if ((c - s) > result) { result = c - s; }
+      break;
+    }
+    while (b < c) {
+      if (*b == *c) { break; }
+      b++;
+    }
+    if (b != c) {
+      s++;
+      b = s;
+      if (s == c) { c++; }
+    } else {
+      if ((c - s + 1) > result) { result = c - s + 1; }
+      c++;
+      b = s;
+    }
+  }
+  return result;
+}
