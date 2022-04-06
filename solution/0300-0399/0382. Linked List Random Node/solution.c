882bcfaa7c588fc7059e0e1890427269615eaaae
jason-xiao <jaSon6787forgItlab>
2022-04-06 20:20:05 +0800

    <0382><c>

---
+++ solution/0300-0399/0382. Linked List Random Node/solution.c
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+typedef struct {
+  struct ListNode* head;
+  int              size;
+} Solution;
+
+Solution* solutionCreate(struct ListNode* head) {
+  Solution* solution = calloc(1, sizeof(Solution));
+  solution->head     = head;
+  solution->size     = 1;
+  while (head->next) {
+    solution->size++;
+    head = head->next;
+  }
+  return solution;
+}
+
+int solutionGetRandom(Solution* obj) {
+  int              index = rand() % obj->size;
+  struct ListNode* tmp   = obj->head;
+  for (int i = 0; i < index; i++) { tmp = tmp->next; }
+  return tmp->val;
+}
+
+void solutionFree(Solution* obj) { free(obj); }
+
+/**
+ * Your Solution struct will be instantiated and called as such:
+ * Solution* obj = solutionCreate(head);
+ * int param_1 = solutionGetRandom(obj);
+
+ * solutionFree(obj);
+*/
