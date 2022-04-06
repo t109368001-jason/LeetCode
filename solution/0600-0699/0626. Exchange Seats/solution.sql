026e4cd697389262788442a6ce0c39cb6e619f8c
jason-xiao <jaSon6787forgItlab>
2022-04-06 21:44:38 +0800

    <0626><sql>

---
+++ solution/0600-0699/0626. Exchange Seats/solution.sql
@@ -0,0 +1,16 @@
+SELECT (
+        CASE
+            WHEN MOD(id, 2) != 0
+            AND counts != id THEN id + 1
+            WHEN MOD(id, 2) != 0
+            AND counts = id THEN id
+            ELSE id - 1
+        END
+    ) AS id,
+    student
+FROM seat,
+    (
+        SELECT COUNT(*) AS countsF
+        FROM seat
+    ) AS seat_counts
+ORDER BY id ASC;
