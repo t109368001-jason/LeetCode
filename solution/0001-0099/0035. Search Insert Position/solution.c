55d7f9cbab66f77f7303232e33ffdf2fdda83c5b
jason-xiao <jaSon6787forgItlab>
2022-04-07 22:30:54 +0800

    <0035><c>

---
+++ solution/0001-0099/0035. Search Insert Position/solution.c
@@ -0,0 +1,23 @@
+
+
+int searchInsert(int* nums, int numsSize, int target) {
+  int* l = nums;
+  int* r = nums + numsSize - 1;
+  int* c = l + (r - l) / 2;
+  if (nums[0] > target) { return 0; }
+  if (nums[numsSize - 1] < target) { return numsSize; }
+  while (l != c && r != c) {
+    printf("%d %d %d\n", l, c, r);
+    if (*c == target) {
+      return c - nums;
+    } else if (*c > target) {
+      r = c;
+      c = l + (r - l) / 2;
+    } else {
+      l = c;
+      c = l + (r - l) / 2;
+    }
+  }
+  if (*c < target) { return c - nums + 1; }
+  return c - nums;
+}
