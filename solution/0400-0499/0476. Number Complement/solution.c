7e405ba5b6b72bf179e60ab0f5afeaec08e5a147
jason-xiao <jaSon6787forgItlab>
2022-04-07 17:53:45 +0800

    <0476><c>

---
+++ solution/0400-0499/0476. Number Complement/solution.c
@@ -0,0 +1,13 @@
+
+
+int findComplement(int num) {
+  int result = 0;
+  int m      = 1;
+  while (1) {
+    if (!(num & 1)) { result += m; }
+    num = num >> 1;
+    if (num <= 0) { break; }
+    m = m << 1;
+  }
+  return result;
+}
