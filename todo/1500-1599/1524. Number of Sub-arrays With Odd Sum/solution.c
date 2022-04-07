fcef43535893c1b541166df3f03738395da354e3
jason-xiao <jaSon6787forgItlab>
2022-04-07 12:26:49 +0800

    <todo><1524><c>

---
+++ todo/1500-1599/1524. Number of Sub-arrays With Odd Sum/solution.c
@@ -0,0 +1,29 @@
+// too slow
+
+int numOfSubarrays(int* arr, int arrSize) {
+  int  result = 0;
+  int* start  = arr - 1;
+  int* end    = arr + arrSize;
+  int* c      = arr;
+  int* l;
+  int* r;
+  // int* nextC = 0;
+  while (c != end) {
+    if (*c & 1) {
+      l = c - 1;
+      r = c + 1;
+      while (l != start && !(*l & 1)) { l--; }
+      int  m    = (c - l);
+      bool flag = true;
+      int  n    = 1;
+      while (r != end) {
+        if (*r++ & 1) { flag = !flag; }
+        if (flag) { n++; }
+        // r++;
+      }
+      result += n * m;
+    }
+    c++;
+  }
+  return result;
+}
