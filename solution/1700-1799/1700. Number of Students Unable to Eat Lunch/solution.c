fd64bd2df375b7a830bc06f7ffa7633501fb1359
jason-xiao <jaSon6787forgItlab>
2022-04-08 00:19:08 +0800

    <1700><c>

---
+++ solution/1700-1799/1700. Number of Students Unable to Eat Lunch/solution.c
@@ -0,0 +1,23 @@
+
+
+int countStudents(int* students, int studentsSize, int* sandwiches, int sandwichesSize) {
+  int* sandwichesEnd = sandwiches + sandwichesSize;
+  int* buf           = calloc(studentsSize, sizeof(int));
+  memcpy(buf, students, sizeof(int) * studentsSize);
+  int remain    = studentsSize;
+  int skipCount = 0;
+  while (skipCount < remain && sandwiches < sandwichesEnd) {
+    if (buf[0] == sandwiches[0]) {
+      remain--;
+      skipCount = 0;
+      sandwiches++;
+      for (int i = 0; i < remain; i++) { buf[i] = buf[i + 1]; }
+    } else {
+      int tmp = buf[0];
+      for (int i = 0; i < remain - 1; i++) { buf[i] = buf[i + 1]; }
+      buf[remain - 1] = tmp;
+      skipCount++;
+    }
+  }
+  return remain;
+}
