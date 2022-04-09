105f938f0ca43c8acf61f8d9bc59c6ece9c1da75
jason-xiao <jaSon6787forgItlab>
2022-04-09 11:06:58 +0800

    <1347><c>

---
+++ solution/1300-1399/1347. Minimum Number of Steps to Make Two Strings Anagram/solution.c
@@ -0,0 +1,18 @@
+
+
+int minSteps(char* s, char* t) {
+  int* countS = calloc(26, sizeof(int));
+  int* countT = calloc(26, sizeof(int));
+  while (*s) {
+    countS[*s - 'a']++;
+    s++;
+  }
+  while (*t) {
+    countT[*t - 'a']++;
+    t++;
+  }
+
+  int result = 0;
+  for (int i = 0; i < 26; i++) { result += abs(countS[i] - countT[i]); }
+  return result / 2;
+}
