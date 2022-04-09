50c710f25064a4213cca7fccb076dac7baaa7165
jason-xiao <jaSon6787forgItlab>
2022-04-09 19:53:33 +0800

    <add> comparePointDistance2DAsc

--- common/compare.h
+++ common/compare.h
@@ -1,3 +1,9 @@
 int compareIntAsc(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
 
 int compareIntDesc(const void* a, const void* b) { return (*(int*)b - *(int*)a); }
+
+int pointDistance2D(int* p) { return p[0] * p[0] + p[1] * p[1]; }
+
+int comparePointDistance2DAsc(const void* a, const void* b) {
+  return (pointDistance2D(*(int**)a) - pointDistance2D(*(int**)b));
+}
