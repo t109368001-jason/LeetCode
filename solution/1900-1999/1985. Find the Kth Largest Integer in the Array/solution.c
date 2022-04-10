2af5d6000d7aa3a502e4aa0a2a85f50343e4aad8
jason-xiao <jaSon6787forgItlab>
2022-04-11 00:57:19 +0800

    <1985><c>

---
+++ solution/1900-1999/1985. Find the Kth Largest Integer in the Array/solution.c
@@ -0,0 +1,19 @@
+int compareNumStrDesc(void* a, void* b) {
+  char* sa = *((char**)a);
+  char* sb = *((char**)b);
+  int   la = strlen(sa);
+  int   lb = strlen(sb);
+  if (la != lb) { return (lb - la); }
+  while (*sa && &sb) {
+    if (*sa != *sb) { return (*sb - *sa); }
+    sa++;
+    sb++;
+  }
+  return 0;
+}
+
+char* kthLargestNumber(char** nums, int numsSize, int k) {
+  qsort(nums, numsSize, sizeof(char*), compareNumStrDesc);
+  return strdup(nums[k - 1]);
+  ;
+}
