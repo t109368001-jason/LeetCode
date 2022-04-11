e0c33b6fa528b9e96b9f4a4b185b7f00b559196f
jason-xiao <jaSon6787forgItlab>
2022-04-11 15:02:41 +0800

    <2145><c>

---
+++ solution/2100-2199/2145. Count the Hidden Sequences/solution.c
@@ -0,0 +1,15 @@
+
+
+int numberOfArrays(int* differences, int differencesSize, int lower, int upper) {
+  long min     = 0;
+  long max     = 0;
+  long current = 0;
+  for (int i = 0; i < differencesSize; i++) {
+    current += differences[i];
+    if (current < min) { min = current; }
+    if (current > max) { max = current; }
+  }
+  int result = (upper - lower) - (max - min) + 1;
+  if (result < 0) { return 0; }
+  return result;
+}
