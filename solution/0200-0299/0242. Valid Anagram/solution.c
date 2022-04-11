d1ff08f1530e4d020b401f1f5ac9514b90720233
jason-xiao <jaSon6787forgItlab>
2022-04-11 22:37:07 +0800

    <0242><c>

---
+++ solution/0200-0299/0242. Valid Anagram/solution.c
@@ -0,0 +1,17 @@
+
+
+bool isAnagram(char* s, char* t) {
+  int* count = calloc(26, sizeof(int));
+  while (*s) {
+    count[*s - 'a']++;
+    s++;
+  }
+  while (*t) {
+    count[*t - 'a']--;
+    t++;
+  }
+  for (int i = 0; i < 26; i++) {
+    if (count[i]) { return false; }
+  }
+  return true;
+}
