be79329e8044278db2949838649275f6887fe7a1
jason-xiao <jaSon6787forgItlab>
2022-04-13 01:32:13 +0800

    <0011><c>

---
+++ solution/0001-0099/0011. Container With Most Water/solution.c
@@ -0,0 +1,18 @@
+
+
+int maxArea(int* height, int heightSize) {
+  int* end = height + heightSize - 1;
+  int  max = 0;
+  while (height < end) {
+    int num = end - height;
+    if (*height > *end) {
+      num *= *end;
+      end--;
+    } else {
+      num *= *height;
+      height++;
+    }
+    if (num > max) { max = num; }
+  }
+  return max;
+}
