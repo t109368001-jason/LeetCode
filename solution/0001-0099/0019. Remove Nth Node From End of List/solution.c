4c16884a4dc7d1551f9b223e6e9be862dd37070c
jason-xiao <jaSon6787forgItlab>
2022-04-14 14:12:09 +0800

    <0019><c>

---
+++ solution/0001-0099/0019. Remove Nth Node From End of List/solution.c
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+  struct ListNode* left  = head;
+  struct ListNode* right = head;
+  for (int i = 0; i < n; i++) { right = right->next; }
+  if (!right) {
+    struct ListNode* tmp = head->next;
+    free(head);
+    return tmp;
+  }
+  while (right->next) {
+    left  = left->next;
+    right = right->next;
+  }
+  printf("%d\n", left->val);
+  struct ListNode* tmp = left->next;
+  // left->val = left->next->val;
+  left->next = left->next->next;
+  free(tmp);
+  return head;
+}
