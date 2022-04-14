b09dddf28cd474a7aa1de9ea63231c1827f53b6d
jason-xiao <jaSon6787forgItlab>
2022-04-14 16:47:41 +0800

    <0075><c>

---
+++ solution/0001-0099/0075. Sort Colors/solution.c
@@ -0,0 +1,38 @@
+void swap(int* a, int* b) {
+  int tmp = *a;
+  *a      = *b;
+  *b      = tmp;
+}
+
+void sortColors(int* nums, int numsSize) {
+  int* right = nums + numsSize - 1;
+
+  while (nums < right) {
+    if (*nums == 0) {
+      nums++;
+      continue;
+    } else if (*right == 2) {
+      right--;
+      continue;
+    } else if (*nums == 2) {
+      swap(nums, right--);
+      continue;
+    } else if (*right == 0) {
+      swap(nums++, right);
+    } else {
+      int* c = nums + 1;
+      while (c < right) {
+        if (*c == 0) {
+          swap(nums, c);
+          break;
+        }
+        if (*c == 2) {
+          swap(c, right);
+          break;
+        }
+        c++;
+      }
+      if (c >= right) { return; }
+    }
+  }
+}
