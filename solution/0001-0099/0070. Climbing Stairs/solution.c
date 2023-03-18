756e9862264ae3b8cd1812ec58dbc409511b635c
jason-xiao <t105360046@ntut.org.tw>
2023-03-19 00:11:27 +0800

    <0070><c>

---
+++ solution/0001-0099/0070. Climbing Stairs/solution.c
@@ -0,0 +1,7 @@
+int climbStairsRecursive(int n, int a, int b) {
+  if (n == 1) { return b; }
+  if (n == 2) { return a + b; }
+  return climbStairsRecursive(n - 1, b, a + b);
+}
+
+int climbStairs(int n) { return climbStairsRecursive(n + 1, 0, 1); }
