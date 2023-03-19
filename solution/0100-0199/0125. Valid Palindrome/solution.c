688d9a586edad503b03efc2638e18869df6d0d75
jason-xiao <t105360046@ntut.org.tw>
2023-03-19 21:57:51 +0800

    <0125><c>

---
+++ solution/0100-0199/0125. Valid Palindrome/solution.c
@@ -0,0 +1,26 @@
+bool isAlphanumeric(char c) {
+  if ('0' <= c && c <= '9') { return true; }
+  if ('A' <= c && c <= 'Z') { return true; }
+  if ('a' <= c && c <= 'z') { return true; }
+  return false;
+}
+
+char lower(char c) {
+  if ('A' <= c && c <= 'Z') { return c - 'A' + 'a'; }
+  return c;
+}
+
+bool isPalindrome(char* s) {
+  char* l = s;
+  char* r = s + strlen(s) - 1;
+
+  while (l < r) {
+    while (l < r && !isAlphanumeric(*l)) { l++; }
+    while (l < r && !isAlphanumeric(*r)) { r--; }
+    if (l >= r) { return true; }
+    if (lower(*l) != lower(*r)) { return false; }
+    l++;
+    r--;
+  }
+  return true;
+}
