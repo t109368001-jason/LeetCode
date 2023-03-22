9e076ce0477d8a8112f9e8e7214f225e123972cf
jason-xiao <t105360046@ntut.org.tw>
2023-03-22 23:04:43 +0800

    <0202><c>

---
+++ solution/0200-0299/0202. Happy Number/solution.c
@@ -0,0 +1,18 @@
+bool isHappy(int n) {
+  if (n == 1) { return true; }
+  if (n == 2 || n == 4) { return false; }
+  int sum = n;
+  int tmp;
+  do {
+    tmp = sum;
+    sum = 0;
+    while (tmp > 0) {
+      int x = tmp % 10;
+      sum += x * x;
+      tmp /= 10;
+    }
+    if (sum == 1) { return true; }
+    if (sum == 2 || sum == 4) { return false; }
+  } while (sum > 1);
+  return true;
+}
