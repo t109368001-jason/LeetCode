77d81c1c63aec4dc05172c3ab9c5103aea9769a5
jason-xiao <t105360046@ntut.org.tw>
2023-03-24 00:01:14 +0800

    <0268><c>

---
+++ solution/0200-0299/0268. Missing Number/solution.c
@@ -0,0 +1,18 @@
+int missingNumber(int* nums, int numsSize) {
+  int sum = (numsSize * (numsSize + 1)) / 2;
+  for (int i = 0; i < numsSize; i++) { sum -= nums[i]; }
+  return sum;
+}
+// first success O(n), O(n)
+// int missingNumber(int* nums, int numsSize){
+//     bool* find = calloc(numsSize + 1, sizeof(bool));
+//     for(int i = 0; i < numsSize; i++) {
+//         find[nums[i]] = true;
+//     }
+//     for(int i = 0; i < numsSize + 1; i++) {
+//         if(!find[i]) {
+//             return i;
+//         }
+//     }
+//     return 0;
+// }
