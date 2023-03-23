78c406e6be70237377b730d953bd2b2beb384bc5
jason-xiao <t105360046@ntut.org.tw>
2023-03-23 16:08:40 +0800

    <0118><c>

---
+++ solution/0100-0199/0118. Pascal's Triangle/solution.c
@@ -0,0 +1,18 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+  int** result       = calloc(numRows, sizeof(int*));
+  *returnSize        = numRows;
+  *returnColumnSizes = calloc(numRows, sizeof(int*));
+  for (int i = 0; i < numRows; i++) {
+    int cols                = i + 1;
+    (*returnColumnSizes)[i] = cols;
+    result[i]               = calloc(cols, sizeof(int));
+    for (int j = 0; j < cols; j++) { result[i][j] = 1; }
+    for (int j = 1; j < cols - 1; j++) { result[i][j] = result[i - 1][j - 1] + result[i - 1][j]; }
+  }
+  return result;
+}
