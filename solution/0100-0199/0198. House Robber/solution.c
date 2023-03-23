f68a38d5950edf2cff4af492ce3fe6062893573c
jason-xiao <t105360046@ntut.org.tw>
2023-03-23 21:14:10 +0800

    <0198><c>

---
+++ solution/0100-0199/0198. House Robber/solution.c
@@ -0,0 +1,20 @@
+int rob(int* nums, int numsSize) {
+  int* buf = calloc(numsSize, sizeof(int));
+  for (int i = 0; i < numsSize; i++) {
+    buf[i]  = nums[i];
+    int max = -1;
+    for (int j = i - 2; j >= i - 3 && j >= 0; j--) {
+      if (buf[j] > max) { max = buf[j]; }
+    }
+    if (max > 0) {
+      // printf("%d, %d, %d\n", i, max, buf[i]);
+      buf[i] += max;
+    }
+  }
+  int max = buf[0];
+  for (int i = 1; i < numsSize; i++) {
+    if (buf[i] > max) { max = buf[i]; }
+  }
+  free(buf);
+  return max;
+}
