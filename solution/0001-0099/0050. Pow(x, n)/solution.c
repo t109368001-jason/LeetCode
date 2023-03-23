41066e82fe622ffdb58522538d5f89bdd1c0ce09
jason-xiao <t105360046@ntut.org.tw>
2023-03-24 00:49:58 +0800

    <0050><c>

---
+++ solution/0001-0099/0050. Pow(x, n)/solution.c
@@ -0,0 +1,10 @@
+double myPow(double x, int n) {
+  if (n == 0 || x == 1.0) { return 1; }
+  if (n == 1) { return x; }
+  if (n < 0) { return 1.0 / x * myPow(1.0 / x, -(n + 1)); }
+  if (n % 2 == 0) {
+    return myPow(x, n / 2) * myPow(x, n / 2);
+  } else {
+    return x * myPow(x, n / 2) * myPow(x, n / 2);
+  }
+}
