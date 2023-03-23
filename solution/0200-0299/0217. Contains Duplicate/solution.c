61633a3590ab465e8c5a438520d2513478b76af7
jason-xiao <t105360046@ntut.org.tw>
2023-03-23 20:26:09 +0800

    <0217><c>

---
+++ solution/0200-0299/0217. Contains Duplicate/solution.c
@@ -0,0 +1,12 @@
+int compareIntDesc(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
+
+bool containsDuplicate(int* nums, int numsSize) {
+  int* buf = calloc(numsSize, sizeof(int));
+  memcpy(buf, nums, numsSize * sizeof(int));
+
+  qsort(buf, numsSize, sizeof(int), compareIntDesc);
+  for (int i = 1; i < numsSize; i++) {
+    if (buf[i - 1] == buf[i]) { return true; }
+  }
+  return false;
+}
